825c4f8d13136953cebc1296c2b45e364f1de7b2
antoningar <antoningaranto@protonmail.com>
2023-06-26 01:27:14 +0100

    feat(tests): add CreateState tests

--- tests/BotGeoGuessrTests/GeoGuessr/States/CreateStateTest.cs
+++ tests/BotGeoGuessrTests/GeoGuessr/States/CreateStateTest.cs
@@ -1,5 +1,6 @@
 ﻿using BotGeoGuessr.GeoGuessr;
 using BotGeoGuessr.GeoGuessr.Services;
+using BotGeoGuessr.GeoGuessr.States;
 using Moq;
 using Serilog;
 
@@ -7,9 +8,50 @@ namespace BotGeoGuessrTests.GeoGuessr.States
 {
     public class CreateStateTest
     {
+        private const string CODE = "joincode";
+        
         private readonly Mock<IGeoGuessrContext> _mockContext = new();
         private readonly Mock<ISeleniumService> _mockSeleniumService = new();
         private readonly Mock<ILogger> _mockLogger = new();
-        
+
+        [Fact]
+        public async Task Execute_ShouldCallGetJoinCode()
+        {
+            _mockSeleniumService
+                .Setup(s => s.GetJoinCode())
+                .Returns(CODE);
+
+            CreateState state = new(_mockContext.Object, _mockSeleniumService.Object, _mockLogger.Object);
+            await state.Execute();
+            
+            _mockSeleniumService.Verify(s => s.GetJoinCode(), Times.Once);
+        }
+
+        [Fact]
+        public async Task Execute_ShouldCallSetJoinCode()
+        {
+            _mockSeleniumService
+                .Setup(s => s.GetJoinCode())
+                .Returns(CODE);
+            _mockContext
+                .Setup(s => s.SetJoinCode(CODE));
+
+            CreateState state = new(_mockContext.Object, _mockSeleniumService.Object, _mockLogger.Object);
+            await state.Execute();
+            
+            _mockContext.Verify(s => s.SetJoinCode(CODE), Times.Once);
+        }
+
+        [Fact]
+        public async Task Execute_ShoulSetNextState()
+        {
+            _mockContext
+                .Setup(s => s.SetCurrentState(It.IsAny<StartState>()));
+
+            CreateState state = new(_mockContext.Object, _mockSeleniumService.Object, _mockLogger.Object);
+            await state.Execute();
+            
+            _mockContext.Verify(s => s.SetCurrentState(It.IsAny<StartState>()), Times.Once);
+        }
     }
 }
