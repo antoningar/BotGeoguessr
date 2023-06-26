fbcb4f1a82b87b2a13cec310285d71c1feef640c
antoningar <antoningaranto@protonmail.com>
2023-06-26 09:43:45 +0100

    feat(tests): add StartState tests

--- tests/BotGeoGuessrTests/GeoGuessr/States/StartStateTest.cs
+++ tests/BotGeoGuessrTests/GeoGuessr/States/StartStateTest.cs
@@ -1,5 +1,7 @@
 ﻿using BotGeoGuessr.GeoGuessr;
 using BotGeoGuessr.GeoGuessr.Services;
+using BotGeoGuessr.GeoGuessr.States;
+using BotGeoGuessr.GeoGuessr.Status;
 using Moq;
 using Serilog;
 
@@ -10,5 +12,37 @@ namespace BotGeoGuessrTests.GeoGuessr.States
         private readonly Mock<IGeoGuessrContext> _mockContext = new();
         private readonly Mock<ISeleniumService> _mockSeleniumService = new();
         private readonly Mock<ILogger> _mockLogger = new();
+
+        [Fact]
+        public async Task Execute_ShouldStartgame()
+        {
+            _mockSeleniumService
+                .Setup(s => s.StartGame());
+
+            StartState state = new(_mockContext.Object, _mockSeleniumService.Object, _mockLogger.Object);
+            await state.Execute();
+            
+            _mockSeleniumService.Verify(s => s.StartGame(), Times.Once);
+        }
+
+        [Fact]
+        public async Task Execute_ShouldSetStatus()
+        {
+            StartState state = new(_mockContext.Object, _mockSeleniumService.Object, _mockLogger.Object);
+            await state.Execute();
+            
+            Assert.Equal(StatusLabels.START_STATUS, state.Status);
+        }
+
+        [Fact]
+        public async Task Execute_ShouldThrowGeoGuessrException()
+        {
+            _mockSeleniumService
+                .Setup(s => s.StartGame())
+                .ThrowsAsync(new GeoGuessrException(""));
+
+            StartState state = new(_mockContext.Object, _mockSeleniumService.Object, _mockLogger.Object);
+            await Assert.ThrowsAsync<GeoGuessrException>(() => state.Execute());
+        }
     }
 }
